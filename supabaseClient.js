11465b593b165095f486650e9cd73b4d887ea1cb
Derrick Auyoung <129267133+derrickauyoung@users.noreply.github.com>
2025-04-15 10:44:52 +1200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 // supabaseClient.js
-import { supabase } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js';
+import { supabase } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js' as base;
 
 const SUPABASE_URL = 'https://napmuiqctvbegldujfbb.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hcG11aXFjdHZiZWdsZHVqZmJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1MzQ1NzYsImV4cCI6MjA2MDExMDU3Nn0.U4SPKOZNpnhhTUzYdiRP_t8O0cAWKrefFrN_ic7jQ6g';
 
-export const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = base.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
