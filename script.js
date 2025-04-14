9c6ac979e64752edcecf64632272324744ad594f
Derrick Auyoung <129267133+derrickauyoung@users.noreply.github.com>
2025-04-14 14:19:14 +1200

    Update script.js

--- script.js
+++ script.js
@@ -121,12 +121,11 @@ document.addEventListener("DOMContentLoaded", () => {
             // Check if end date is past
             const end_date = document.createElement("div");
             end_date.className = "end-date";
-            end_date_text = `<p><strong>Auction ended:</strong> $${item.end_date}</p>`;
+            end_date_text = `<p><strong>Auction ended:</strong> ${item.end_date}</p>`;
             end_date.innerHTML = end_date_text;
 
-            console.info("End date: ", item.end_date.Timestamp);
-            console.info("Now: ", Date.now());
-            if (item.end_date.Timestamp > Date.now()) {
+            const timestamptzMillis = new Date(item.end_date).getTime();
+            if (timestamptzMillis > Date.now()) {
                 card.appendChild(button);
             }
             else {
