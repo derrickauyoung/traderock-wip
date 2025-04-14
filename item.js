85fd223332441d628e1564f62cb3599932c86b95
Derrick Auyoung <derrick.auyoung@gmail.com>
2025-04-14 23:45:16 +1200

    fixed import from render.js

--- item.js
+++ item.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js'
-import { renderItem } from './script.js';
+import { renderItem } from './render.js';
 
 const supabaseUrl = 'https://napmuiqctvbegldujfbb.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hcG11aXFjdHZiZWdsZHVqZmJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1MzQ1NzYsImV4cCI6MjA2MDExMDU3Nn0.U4SPKOZNpnhhTUzYdiRP_t8O0cAWKrefFrN_ic7jQ6g';
