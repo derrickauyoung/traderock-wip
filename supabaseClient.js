1cc517ba72887008c1cf3e6dea25f788370df1d8
Derrick Auyoung <129267133+derrickauyoung@users.noreply.github.com>
2025-04-15 10:55:29 +1200

    js syntax for import as argh

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 // supabaseClient.js
-import { supabase } as base from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js';
+import { supabase as base } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://napmuiqctvbegldujfbb.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hcG11aXFjdHZiZWdsZHVqZmJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1MzQ1NzYsImV4cCI6MjA2MDExMDU3Nn0.U4SPKOZNpnhhTUzYdiRP_t8O0cAWKrefFrN_ic7jQ6g';
