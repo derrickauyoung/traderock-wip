7c84c3f25db53e266a56ae0071fc9cec016ea053
Derrick Auyoung <129267133+derrickauyoung@users.noreply.github.com>
2025-04-15 10:18:25 +1200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,4 +2,4 @@
 const SUPABASE_URL = 'https://napmuiqctvbegldujfbb.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hcG11aXFjdHZiZWdsZHVqZmJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1MzQ1NzYsImV4cCI6MjA2MDExMDU3Nn0.U4SPKOZNpnhhTUzYdiRP_t8O0cAWKrefFrN_ic7jQ6g';
 
-export const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
