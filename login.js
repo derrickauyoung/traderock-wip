04d6cc69cf7b642ae13d449a851060a2a28a58ca
Derrick Auyoung <derrick.auyoung@gmail.com>
2025-04-14 18:13:55 +1200

    global access to supabase in login

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-const supabase = supabase.createClient(
+const supabase = window.supabase.createClient(
     "https://napmuiqctvbegldujfbb.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hcG11aXFjdHZiZWdsZHVqZmJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1MzQ1NzYsImV4cCI6MjA2MDExMDU3Nn0.U4SPKOZNpnhhTUzYdiRP_t8O0cAWKrefFrN_ic7jQ6g"
 );
