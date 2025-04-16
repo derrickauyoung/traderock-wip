00e6962d234bb873d13578591cf18a99a937de3c
Derrick Auyoung <derrick.auyoung@gmail.com>
2025-04-16 23:51:45 +1200

    added auth token to verify header

--- script/verify-captcha.js
+++ script/verify-captcha.js
@@ -1,9 +1,11 @@
+
 export async function verifyCaptcha(token) {
     const res = await fetch("https://napmuiqctvbegldujfbb.supabase.co/functions/v1/verify-captcha", {
       method: "POST",
       body: JSON.stringify({ token }),
       headers: {
         "Content-Type": "application/json",
+        'Authorization': `Bearer ${token}`,
       }
     });
   
