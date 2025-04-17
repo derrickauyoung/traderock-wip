46783948fc4112abe721cbf677094cfd2d2e63b5
Derrick Auyoung <derrick.auyoung@gmail.com>
2025-04-18 10:55:18 +1200

    use proper fetch address for edge function

--- script/email.js
+++ script/email.js
@@ -2,7 +2,7 @@
 
 export async function sendMail(buyerEmail, sellerName, itemTitle, price, ccEmails=[]) {
 
-    await fetch(`${location.origin}/functions/v1/send-confirmation-email`, {
+    await fetch("https://napmuiqctvbegldujfbb/functions/v1/send-confirmation-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
